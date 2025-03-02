b128c73211dc1a06ba47a292113f79ac958c6b1a
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2025-03-03 01:06:39 +0300

    trying add auto update

--- client/src/widgets/ui/monitor/monitor.tsx
+++ client/src/widgets/ui/monitor/monitor.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Search, Table } from "../../../features/stock"
 import { Title } from "../../../shared/ui/title"
 import { useForm } from "react-hook-form"
@@ -8,7 +8,8 @@ type SearchType = {
 }
 
 export const Monitor = () => {
-  const [table, setTable] = useState<string>("")
+  const [search, setSearch] = useState<string>("")
+  const [count, setCount] = useState<number>(0)
 
   const { handleSubmit, control } = useForm<SearchType>({
     mode: "onChange",
@@ -17,16 +18,27 @@ export const Monitor = () => {
     },
   })
 
-  const onSubmit = async (data: SearchType) => {
-    setTable(data.search)
+  useEffect(() => {
+    // auto call function with handle ?? don't work because search string not edit ?? need work inside table
+    const timer = setTimeout(() => {
+      handleSubmit(onSubmit)
+      setCount(count + 1)
+      console.log("execute timeout", count)
+    }, 5000)
+    return () => clearTimeout(timer)
+  }, [count, handleSubmit])
+
+  const onSubmit = (data: SearchType) => {
+    setSearch(data.search)
   }
+
   return (
     <main className="grow-1">
       <section className="flex flex-col gap-4">
         <Title title="Stock Monitor" />
         <Search onSubmit={handleSubmit(onSubmit)} control={control} />
 
-        {table !== "" ? <Table search={table} /> : <Table search="MSFT" />}
+        {search !== "" ? <Table search={search} /> : <Table search="MS" />}
       </section>
     </main>
   )
